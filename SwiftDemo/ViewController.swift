b0b70a85ad07b527998e315c2536d695f20b8b7c
zhaobingxu <zhaobingxu@everyoo.com>
2016-02-19 08:42:33 +0800

    Initial Commit

---
+++ SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by Architect_ZBX on 16/2/19.
+//  Copyright © 2016年 zhaobingxu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
